bb2eb53daf9d94b602be19304c506e189c51bca1
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2025-12-10 11:54:53 +0700

    add chat controller

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\MessageSent;
+use App\Models\ChatMessage;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ChatController extends Controller
+{
+    public function index() {
+         $chats = ChatMessage::with('user:id,name')->get()->map(function ($chat) {
+            return [
+                'id' => $chat->id,
+                'message' => $chat->message,
+                'user_name' => $chat->user->name,
+                'created_at' => $chat->created_at->diffForHumans(),
+            ];
+        });
+
+        return Inertia::render('Chat/Index', [
+            'chats' => $chats,
+        ]);
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'message' => 'required|string|',
+        ]);
+
+        $chatMessage = $request->user()->chatMessages()->create($validated);
+
+        MessageSent::broadcast($chatMessage);
+
+        return redirect()->back();
+    }
+}
